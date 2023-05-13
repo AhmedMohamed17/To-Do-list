0fa0dda0f4c989631de1b12e1b6e39204658b1c8
unknown <ahmedmohamed7sayed@gmail.com>
2023-05-13 05:26:26 +0300

    Update SearchHandler and add LocalStorage :eyes:

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,46 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Item } from "./item";
 
+/// The Base App
 function App() {
-  /// The Base App
   const ToDoList = [
     { id: 1, task: "finish your tasks with nazmy" },
     { id: 2, task: "meeting with your manager AbdelRhman" },
     { id: 3, task: "PS in CodeForce with osama khalil" },
   ];
-  ////////  using useState to Store Data
-  const [itemlist, SetItemList] = useState(ToDoList);
-  const [state, setState] = useState(null);
 
+  ////////  using useState to Store Data
+  const [itemlist, SetItemList] = useState([]);
+  const [state, setState] = useState(null);
+  const [search, setSearch] = useState(null);
+
+  ////// local storage
+  let localData = localStorage.getItem("localData");
+  localData = JSON.parse(localData);
+  useEffect(() => {
+    if (localData) {
+      SetItemList(localData);
+    } else {
+      SetItemList(ToDoList);
+    }
+  }, []);
+  // search input Function
   const OnSearchHandler = (e) => {
-    // search input Function
     let searchTerm = e.target.value.trim().toLowerCase();
     if (searchTerm === "") {
-      console.log("the search");
+      setSearch(null);
     } else {
-      let searchItems = itemlist.filter((todo) =>
-        todo.task.toLowerCase().includes(searchTerm)
+      let filteredList = itemlist.filter((item) =>
+        item.task.toLowerCase().includes(searchTerm)
       );
-      SetItemList(searchItems);
+      setSearch(filteredList);
     }
   };
+  // pass data from input to state
   const onChangeHandler = (e) => {
-    // pass data from input data tag to state
     setState(e.target.value);
   };
+  // Submit Data Function
   const onhandlesubmit = (e) => {
-    // Submit Data Function
     e.preventDefault();
     let id = Math.random();
     SetItemList([...itemlist, { id, task: state }]);
+    e.target.reset();
   };
+  // Delete Function
   const onclickhandler = (item) => {
-    // Delete Function
     let changeitems = [...itemlist];
     changeitems = changeitems.filter((e) => e.id !== item.id);
     SetItemList(changeitems);
   };
+  /// update localstorage
+  useEffect(() => {
+    localStorage.setItem("localData", JSON.stringify(itemlist));
+  }, [itemlist]);
 
   return (
     <div>
@@ -55,7 +72,7 @@ function App() {
           />
           <div className="main query">
             <ul>
-              <Item items={itemlist} ONCliCK={onclickhandler} />
+              <Item items={search || itemlist} ONCliCK={onclickhandler} />
             </ul>
           </div>
           <div>
