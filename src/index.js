e83cbabe1e146a144e9d79841f913df18a3a172b
unknown <ahmedmohamed7sayed@gmail.com>
2023-05-13 07:10:25 +0300

    update to deploy production

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
-
-// import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
